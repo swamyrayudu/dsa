1b4582431d9378e72c43e8370e2e9a97d858334d
Rayudu Veera Venkata Swamy <123217580+swamyrayudu@users.noreply.github.com>
2026-06-30 10:36:41 +0530

    Time: 900 ms (42.76%), Space: 33.7 MB (64.93%) - LeetHub

--- 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
+++ 1911-maximum-alternating-subsequence-sum/1911-maximum-alternating-subsequence-sum.py
@@ -1,15 +1,16 @@
 class Solution:
     def maxAlternatingSum(self, nums: List[int]) -> int:
         n = len(nums)
-        dp = [[0 for i in range(2)] for j in range(n+1)]
-
+        pre = [0] * 2
         for i in range(n-1,-1,-1):
+            curr = [0] * 2
             for parity in range(2):
-                not_pick = dp[i+1][parity]
+                not_pick = pre[parity]
                 pick = 0
                 if parity == 0:
-                    pick = nums[i] + dp[i+1][1]
+                    pick = nums[i] + pre[1]
                 else:
-                    pick = -nums[i] + dp[i+1][0]
-                dp[i][parity] = max(not_pick,pick)
-        return dp[0][0]
+                    pick = -nums[i] + pre[0]
+                curr[parity] = max(not_pick,pick)
+            pre = curr
+        return pre[0]
