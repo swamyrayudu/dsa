8d6c980037729a8f28a253644b34b481d22956a4
Rayudu Veera Venkata Swamy <123217580+swamyrayudu@users.noreply.github.com>
2026-06-30 13:01:59 +0530

    Time: 46 ms (93.72%), Space: 55.9 MB (81.68%) - LeetHub

--- 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
+++ 2140-solving-questions-with-brainpower/2140-solving-questions-with-brainpower.py
@@ -1,14 +1,12 @@
 class Solution:
     def mostPoints(self, questions: List[List[int]]) -> int:
         n = len(questions)
-        dp = [-1 for i in range(n)]
-        def f(i):
-            if i >= n:
-                return 0
-            if dp[i] != -1:
-                return dp[i]
-            not_pick = f(i+1)
-            pick = questions[i][0] + f(i+questions[i][1]+1)
-            dp[i] = max(not_pick,pick)
-            return dp[i]
-        return f(0)
+        dp = [0] * (n+1)
+        for i in range(n-1,-1,-1):
+            not_pick = dp[i+1]
+            next_ind = i + questions[i][1] + 1
+            pick = questions[i][0]
+            if next_ind <= n:
+                pick+=dp[next_ind]
+            dp[i] = max(pick,not_pick)
+        return dp[0]
