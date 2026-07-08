569ad8a7ddaeed6aa3c973dbadcb193529aa0c91
Rayudu Veera Venkata Swamy <123217580+swamyrayudu@users.noreply.github.com>
2026-07-08 18:53:05 +0530

    Time: 0 ms (100%), Space: 19.2 MB (69.93%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.py
+++ 0091-decode-ways/0091-decode-ways.py
@@ -1,18 +1,15 @@
 class Solution:
     def numDecodings(self, s: str) -> int:
         n = len(s)
-        dp = [-1] * (n)
-        def f(i):
-            if i == n:
-                return 1
+        dp = [0] * (n+1)
+        dp[n] = 1
+        for i in range(n-1,-1,-1):
             if s[i] == '0':
-                return 0
-            if dp[i] != -1:
-                return dp[i]
-            one_digit = f(i+1)
+                dp[i] = 0
+                continue
+            one_digit = dp[i+1]
             two_digit = 0
-            if i+1 < n and 10<=int(s[i:i+2])<= 26:
-                two_digit = f(i+2)
+            if i + 1 < n and 10<=int(s[i:i+2])<=26:
+                two_digit = dp[i+2]
             dp[i] = one_digit+two_digit
-            return dp[i]
-        return f(0)
+        return dp[0]
