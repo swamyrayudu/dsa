89f636902f583f59f47f1941f792c7631feb9ced
Rayudu Veera Venkata Swamy <123217580+swamyrayudu@users.noreply.github.com>
2026-07-24 13:23:41 +0530

    Time: 6 ms (71.6%), Space: 20.8 MB (61.67%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -5,15 +5,9 @@ class Solution:
         for num in nums:
             earn[num] += num
         n = len(earn)
-        dp = [-1] * (n+1)
-        def f(i):
-            if i >= n:
-                return 0
-            if dp[i] != -1:
-                return dp[i]
-            not_pick = 0 + f(i+1)
-            pick = earn[i] + f(i+2)
-            dp[i] = max(not_pick,pick)
-            return dp[i]
-            
-        return f(0)
+        dp = [0] * (n+2)
+        for i in range(n-1,-1,-1):
+            not_pick = 0 + dp[i+1]
+            pick = earn[i] + dp[i+2]
+            dp[i] = max(pick,not_pick)
+        return dp[0]
