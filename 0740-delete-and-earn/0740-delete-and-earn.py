3f790de6c6ca560493c994730f450a9a7a0435a2
Rayudu Veera Venkata Swamy <123217580+swamyrayudu@users.noreply.github.com>
2026-07-24 13:09:51 +0530

    Time: 11 ms (35.19%), Space: 23.5 MB (13.08%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -1,20 +1,19 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        n = len(nums)
-        max_num = max(nums)
-        earn = [0] * (max_num+1)
+        maxnum = max(nums)
+        earn = [0] * (maxnum+1)
         for num in nums:
             earn[num] += num
-        dp = [-1 for i in range(max_num+1)]
+        n = len(earn)
+        dp = [-1] * (n+1)
         def f(i):
-            if i < 0:
+            if i >= n:
                 return 0
-            if i == 0:
-                return earn[0]
             if dp[i] != -1:
                 return dp[i]
-            not_pick = f(i-1)
-            pick = earn[i] + f(i-2)
+            not_pick = 0 + f(i+1)
+            pick = earn[i] + f(i+2)
             dp[i] = max(not_pick,pick)
             return dp[i]
-        return f(max_num)
+            
+        return f(0)
