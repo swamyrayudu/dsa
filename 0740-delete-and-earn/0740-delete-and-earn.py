50e5f12eb71490442d253a86c87ac9ba174daf8f
Rayudu Veera Venkata Swamy <123217580+swamyrayudu@users.noreply.github.com>
2026-07-24 13:25:32 +0530

    Time: 7 ms (67.27%), Space: 20.5 MB (82.47%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -5,9 +5,12 @@ class Solution:
         for num in nums:
             earn[num] += num
         n = len(earn)
-        dp = [0] * (n+2)
+        pre1 = 0
+        pre2 = 0
         for i in range(n-1,-1,-1):
-            not_pick = 0 + dp[i+1]
-            pick = earn[i] + dp[i+2]
-            dp[i] = max(pick,not_pick)
-        return dp[0]
+            not_pick = 0 + pre1
+            pick = earn[i] + pre2
+            curr = max(pick,not_pick)
+            pre2 = pre1
+            pre1 = curr
+        return pre1
