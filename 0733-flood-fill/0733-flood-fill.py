98e93d565a189135ee418a67e4c7b6d3f9cdeb32
Rayudu Veera Venkata Swamy <123217580+swamyrayudu@users.noreply.github.com>
2026-07-27 14:14:42 +0530

    Time: 0 ms (100%), Space: 19.8 MB (16.56%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.py
+++ 0733-flood-fill/0733-flood-fill.py
@@ -5,18 +5,16 @@ class Solution:
 	        return image
 	    m = len(image)
 	    n = len(image[0])
-	    vis = [[False for i in range(n)] for j in range(m)]
 	    dfsrow = [0,1,-1,0]
 	    dfscol = [1,0,0,-1]
 	    def dfs(row,col):
-	        vis[row][col] = True
 	        image[row][col] = color
 	        
 	        for i in range(4):
 	            newrow = dfsrow[i] + row
 	            newcol = dfscol[i] + col
 	            
-	            if 0 <= newrow < m and 0 <= newcol < n and image[newrow][newcol] == original and not vis[newrow][newcol]:
+	            if 0 <= newrow < m and 0 <= newcol < n and image[newrow][newcol] == original:
 	                dfs(newrow,newcol)
 	    dfs(sr,sc)
 	    return image
